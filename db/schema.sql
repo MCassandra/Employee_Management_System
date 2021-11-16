10029b4ace152bf46417c10240a80c1512708ae6
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-11-16 06:52:55 -0800

    fix schema

--- db/schema.sql
+++ db/schema.sql
@@ -18,5 +18,7 @@ id INT AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL,
-manager_id INT
+manager_id IN
+CONSTRAINT fk_role FOREIGN KEY (role) REFERENCES role(id),
+CONSTRAINT manager FOREIGN KEY (manager) REFERENCES employee(id)
 );
