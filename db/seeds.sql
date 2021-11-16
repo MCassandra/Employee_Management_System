735e5aefcc524e41c1870c3e21b2072041c2d87b
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-11-16 06:50:27 -0800

    inserting data in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES
+    ("Sample Processing"),
+    ("Parasitology"),
+    ("Chemisty"),
+    ("Hematology");
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ("Lead", 100000, 1),
+    ("Lab Technician 3", 90000, 2),
+    ("Lab Technician 2", 80000, 2),
+    ("Lab Technician 1", 70000, 2,)
+    ("Assistant", 60000, 1);
+ INSERT INTO employee (first_name, last_name, role_id, manager_id)
+ VALUES
+    ("Alley", "Apples", 1, NUll),
+    ("Bruce", "Barnes", 2, NUll),
+    ("Cathy", "Cuach", 3, 2),
+    ("Daniel", "DeSherpa", 5, NUll),
+    ("Elliza", "Enririquez", 3, 2);
